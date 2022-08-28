e76eacb65e42e4f15e0a43ee696e557a4c6816f6
trustedchild <tguwor@gmail.com>
2022-08-28 04:33:44 -0400

    added UserRole model

---
+++ src/main/java/com/revature/servlets/models/UserRole.java
@@ -0,0 +1,41 @@
+package com.revature.servlets.models;
+
+public class UserRole {
+
+    private String role_id;
+    private String role;
+
+    public UserRole() {
+    }
+
+    public UserRole(String role_id, String role) {
+        this.role_id = role_id;
+        this.role = role;
+    }
+
+    public String getRole_id() {
+        return role_id;
+    }
+
+    public void setRole_id(String role_id) {
+        this.role_id = role_id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "UserRole{" +
+                "role_id='" + role_id + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+
+
+}
