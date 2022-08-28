ba43d2e3c283025244a479c580e446bbb045738b
trustedchild <tguwor@gmail.com>
2022-08-28 04:24:07 -0400

    added user model

---
+++ src/main/java/com/revature/servlets/models/User.java
@@ -0,0 +1,107 @@
+package com.revature.servlets.models;
+
+public class User {
+
+    private String user_id;
+    private String username;
+    private String email;
+    private String password;
+    private String given_name;
+    private String surname;
+    private boolean is_active;
+    private String role_id;
+
+    public User() {
+    }
+
+    public User(String user_id, String username, String email, String password, String given_name, String surname, boolean is_active, String role_id) {
+        this.user_id = user_id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.given_name = given_name;
+        this.surname = surname;
+        this.is_active = is_active;
+        this.role_id = role_id;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getGiven_name() {
+        return given_name;
+    }
+
+    public void setGiven_name(String given_name) {
+        this.given_name = given_name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public boolean isIs_active() {
+        return is_active;
+    }
+
+    public void setIs_active(boolean is_active) {
+        this.is_active = is_active;
+    }
+
+    public String getRole_id() {
+        return role_id;
+    }
+
+    public void setRole_id(String role_id) {
+        this.role_id = role_id;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "user_id='" + user_id + '\'' +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", given_name='" + given_name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", is_active=" + is_active +
+                ", role_id='" + role_id + '\'' +
+                '}';
+    }
+
+
+}
